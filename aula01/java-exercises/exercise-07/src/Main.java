24dc24a29342a80b7e2608514c44d08c2611f025
eduardozamit <zamit.eduardo@gmail.com>
2023-09-12 11:17:44 -0300

    Diminuição de linhas de código + polimento

--- aula01/java-exercises/exercise-07/src/Main.java
+++ aula01/java-exercises/exercise-07/src/Main.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello and welcome! You are in the 07 exercise of class-01");
-        System.out.println("------------------------------------------------------------");
-
         int age;
         int salary;
 
