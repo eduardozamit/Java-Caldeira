47e611ca7ae5245ebec045fa0437241d6a3c2f74
eduardozamit <zamit.eduardo@gmail.com>
2023-09-14 15:40:30 -0300

    adicionando exercicios para terminar em casa

---
+++ aula03/java-exercises/exercises-01/src/ContaBancaria.java
@@ -0,0 +1,9 @@
+public class ContaBancaria {
+    private String nome;
+    private String cpf;
+    private int identificadorConta;
+    private String banco;
+    private String endereco;
+    private double saldo;
+    
+}
