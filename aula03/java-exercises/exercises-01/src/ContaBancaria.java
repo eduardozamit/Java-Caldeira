7c0b941a91c6822c2bad7e86fbf635de4c58aa86
eduardozamit <zamit.eduardo@gmail.com>
2023-09-19 16:10:51 -0300

    atualizacao aula 4

--- aula03/java-exercises/exercises-01/src/ContaBancaria.java
+++ aula03/java-exercises/exercises-01/src/ContaBancaria.java
@@ -5,5 +5,32 @@ public class ContaBancaria {
     private String banco;
     private String endereco;
     private double saldo;
-    
+
+    public void saque(double valor) {
+        if (saldo < valor) {
+            System.out.println("Saldo insuficiente!");
+        } else {
+            saldo -= valor;
+        }
+
+    }
+
+    public void deposito(double valor) {
+        if (valor < 0) {
+            System.out.println("Impossivel depositar uma valor menor que zero!");
+        } else {
+            saldo += valor;
+        }
+    }
+
+    public void pix(double valor) {
+        if (valor < saldo) {
+            System.out.println("Vocë nao pode tranferir um pix negativo");
+            saldo -= valor;
+        }
+    }
+
+    public void transferencia(ContaBancaria destino, double valor) {
+        
+    }
 }
