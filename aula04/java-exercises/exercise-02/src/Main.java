568e8bb82676a50b7c4f74b08a147dea824c548a
eduardozamit <zamit.eduardo@gmail.com>
2023-09-25 23:18:00 -0300

    atualização exercicios

--- aula04/java-exercises/exercise-02/src/Main.java
+++ aula04/java-exercises/exercise-02/src/Main.java
@@ -4,6 +4,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<FormaGeometrica> formas = new ArrayList<>();
-      
+
+        formas.add(new Quadrado(3));
+        formas.add(new Circulo(6));
+        formas.add(new Retangulo(2, 4));
+        formas.add(new Triangulo(8, 7));
+        formas.add(new Losango(3, 6));
+
+        //metodo for each
+        for (FormaGeometrica forma : formas) {
+            System.out.println(forma.calcularArea());
+        }
     }
 }
